8751b63c314cb9079e58c30548073445e8bb8d76
Ben Hopkins <bjhops17@gmail.com>
2021-06-14 14:18:29 -0700

    test

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,41 @@
 const PlantDB = "Plants";
+let db;
 
-var request = indexedDB.open(PlantDB, 2);
+var request = window.indexedDB.open(PlantDB, 1);
 
 request.onerror = function(event) {
   console.log(event)
 };
 request.onupgradeneeded = function(event) {
-  var db = event.target.result;
+    console.log(event.target)
+  db = event.target.result;
 
   // Create an objectStore to hold information about our customers. We're
   // going to use "ssn" as our key path because it's guaranteed to be
   // unique - or at least that's what I was told during the kickoff meeting.
-  var objectStore = db.createObjectStore("plant", { keyPath: "index" });
-
+  const objectStore = db.createObjectStore("offLine", { autoIncrement: true});
+  
   // Create an index to search customers by name. We may have duplicates
   // so we can't use a unique index.
   objectStore.createIndex("name", "name", { unique: false });
-  objectStore.createIndex("img", "img", { unique: true });
-  objectStore.createIndex("note", "note", { unique: true });
+  objectStore.createIndex("img", "img", { unique: false});
+  objectStore.createIndex("note", "note", { unique: false });
 
 
   // Use transaction oncomplete to make sure the objectStore creation is
   // finished before adding data into it.
-  objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    customerData.forEach(function(customer) {
-      customerObjectStore.add(customer);
-    });
-  };
 };
+
+// add
+const handleClick = (event) => {
+    console.log(db)
+    const transaction = db.transaction(["offLine"], "readwrite").objectStore("offLine")
+    const accessObjectStore = transaction.add({name: "dandylions", img: "urlforimag", note: "this is a note"})
+}
+// GetAll
+const handleOtherClick = (event) => {
+    const transaction = db.transaction(["offLine"], "readwrite").objectStore("offLine")
+    let plants = transaction.getAll()
+    console.log(plants)
+}
+    
